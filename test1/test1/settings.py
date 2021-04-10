53d42cf77edc8124081faef1cee142156f061e19
jingweiyu <weiyu_strive@163.com>
2021-04-10 22:05:58 +0800

    静态资源的设置和访问

--- test1/test1/settings.py
+++ test1/test1/settings.py
@@ -138,6 +138,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# 告知系统，我们的图片在哪
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
