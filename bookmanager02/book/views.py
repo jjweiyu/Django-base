19f5bdf9cce83b750cfa12b113e1b28d8074e652
jingweiyu <weiyu_strive@163.com>
2021-04-13 16:36:02 +0800

    '从request请求中获取form表单及json数据'

--- bookmanager02/book/views.py
+++ bookmanager02/book/views.py
@@ -28,6 +28,36 @@ def shop(request, city_id, shop_id):
     # 当含有多个值的时候需要用 getlist 来获取键值
     return HttpResponse("小饭店")
 
+def register(request):
+    # 获取 form表单数据
+    data = request.POST
+    print(data)
+    # <QueryDict: {'username': ['jing'], 'password': ['123']}>
+    return HttpResponse("ok")
+
+def json(request):
+    # json数据不能通过 request.POST获取,需要通过 request.body接收
+    body = request.body
+    # print(body)
+    # b'{\r\n    "name":"jing",\r\n    "age":18\r\n}'
+    body_str = body.decode()
+    print(body_str)
+    # 字符串类型的数据
+    # {
+    #     "name":"jing",
+    #     "age":18
+    # }
+    # JSON形式的字符串可以转换为 python的字典
+    import json
+    body_dict = json.loads(body_str)
+    print(body_dict)
+
+    ###########请求头##########
+    # 获取请求头信息   字典类型 
+    # print(request.META)
+    print(request.META.get('SERVER_PORT'))
+    return HttpResponse("json")
+
 
 """
 查询字符串
